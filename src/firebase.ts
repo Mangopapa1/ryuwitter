1004d8e80b096718acec54aa12bb8ebbd3c3de58
Mangopapa1 <gustp26@gmail.com>
2023-10-19 15:00:24 +0900

    📦 firebase 설치

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCN5JAYX_m6lR1hobzEUQ8wSqaoxNK1vD4",
+  authDomain: "ryuwitter-reloaded.firebaseapp.com",
+  projectId: "ryuwitter-reloaded",
+  storageBucket: "ryuwitter-reloaded.appspot.com",
+  messagingSenderId: "506668713414",
+  appId: "1:506668713414:web:2e98a2f0452172e9e51ec7",
+};
+
+const app = initializeApp(firebaseConfig);
